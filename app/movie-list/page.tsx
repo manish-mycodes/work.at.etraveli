ffc698466c4561df276dca2d378d667c53c96c2f
Manish Kumar <manish.mycodes@gmail.com>
2024-11-30 21:44:31 +0530

    E-Traveli: Basic Next.js Setup

---
+++ app/movie-list/page.tsx
@@ -0,0 +1,9 @@
+import SearchBar from '../components/searchBar'
+
+export default function MovieList() {
+    return (
+        <div className="flex flex-col">
+            <SearchBar />
+        </div>
+    )
+}
